79460369a46e4c97cc333e229acf804eeaf3ad19
Stéphane Caron <stephane.caron@normalesup.org>
2022-04-16 10:40:42 +0200

    Introduce KeyError similar to Python dict's

--- include/palimpsest/exceptions.h
+++ include/palimpsest/exceptions.h
@@ -83,4 +83,32 @@ class TypeError : public PalimpsestError {
   ~TypeError() throw() {}
 };
 
+//! Exception thrown when a dictionary key is not found.
+class KeyError : public PalimpsestError {
+ public:
+  /*! Create a key error.
+   *
+   * \param[in] key Key that was not found.
+   * \param[in] file Source file of the instruction that threw the error.
+   * \param[in] line Line of code in that file where the throw originates from.
+   * \param[in] message Error message.
+   */
+  KeyError(const std::string& key, const std::string& file, unsigned line,
+           const std::string& message)
+      : PalimpsestError(
+            file, line,
+            std::string("Key \"") + key + "\" not found. " + message),
+        key_(key) {}
+
+  //! Empty destructor
+  ~KeyError() throw() {}
+
+  //! Key that was not found
+  const std::string& key() const throw() { return key_; }
+
+ private:
+  //! Key that was not found
+  std::string key_;
+};
+
 }  // namespace palimpsest
