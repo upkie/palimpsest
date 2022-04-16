958e28ccc5740dd11914451a1db5dcf99d87d886
Stéphane Caron <stephane.caron@normalesup.org>
2022-04-16 10:40:14 +0200

    Document and change order of PalimpsestError args

--- include/palimpsest/exceptions.h
+++ include/palimpsest/exceptions.h
@@ -24,21 +24,29 @@ class PalimpsestError : public std::runtime_error {
  public:
   /*! Create a new error.
    *
+   * \param[in] file Source file of the instruction that threw the error.
+   * \param[in] line Line of code in that file where the throw originates from.
    * \param[in] message Error message.
    */
-  PalimpsestError(const std::string& message, const std::string& file,
-                  unsigned line)
+  PalimpsestError(const std::string& file, unsigned line,
+                  const std::string& message)
       : std::runtime_error(message) {
     std::ostringstream out;
     out << "[" << file << ":" << line << "] " << message;
     message_ = out.str();
   }
 
+  /*! Copy an existing error, adding to the error message.
+   *
+   * \param[in] other Existing error.
+   * \param[in] extra_message Additional error message.
+   */
   PalimpsestError(const PalimpsestError& other,
                   const std::string& extra_message)
       : std::runtime_error(other.message_ + extra_message),
         message_(other.message_ + extra_message) {}
 
+  //! Empty destructor
   ~PalimpsestError() throw() {}
 
   //! Error message
@@ -54,17 +62,24 @@ class PalimpsestError : public std::runtime_error {
  */
 class TypeError : public PalimpsestError {
  public:
-  /*! Create a key error.
+  /*! Create a type error.
    *
-   * \param[in] key Key that was not found.
+   * \param[in] file Source file of the instruction that threw the error.
+   * \param[in] line Line of code in that file where the throw originates from.
    * \param[in] message Error message.
    */
   TypeError(const std::string& file, unsigned line, const std::string& message)
-      : PalimpsestError(message, file, line) {}
+      : PalimpsestError(file, line, message) {}
 
+  /*! Copy an existing error, adding to the error message.
+   *
+   * \param[in] other Existing error.
+   * \param[in] extra_message Additional error message.
+   */
   TypeError(const TypeError& other, const std::string& extra_message)
       : PalimpsestError(other, extra_message) {}
 
+  //! Empty destructor
   ~TypeError() throw() {}
 };
 
