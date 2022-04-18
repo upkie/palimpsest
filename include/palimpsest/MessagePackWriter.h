c36d5b43c0073ee5f0f24ae85691bc6035b3512b
Stéphane Caron <stephane.caron@normalesup.org>
2022-04-18 13:22:12 +0200

    WIP: MessagePackWriter class to serialize to buffer

---
+++ include/palimpsest/MessagePackWriter.h
@@ -0,0 +1,255 @@
+/*
+ * Copyright 2022 Stéphane Caron
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * This file incorporates work covered by the following copyright and
+ * permission notice:
+ *
+ *     MessagePackBuilder from https://github.com/jrl-umi3218/mc_rtc/
+ *     Copyright 2015-2019 CNRS-UM LIRMM, CNRS-AIST JRL
+ *     License: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <mpack.h>
+
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+#include <array>
+#include <exception>
+#include <map>
+#include <memory>
+#include <set>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+namespace palimpsest {
+
+/*! Write MessagePack (using MPack's Write API) to a vector of bytes.
+ *
+ * \note After a message has been written, the writer instance must be
+ * discarded to create a new message.
+ */
+class MessagePackWriter {
+ public:
+  /*! Constructor.
+   *
+   * \param buffer Buffer used to store the data. It may grow if needed.
+   */
+  explicit MessagePackWriter(std::vector<char> &buffer);
+
+  /*! Destructor. */
+  ~MessagePackWriter();
+
+  /*! Add data to the MessagePack (basic)
+   *
+   * These overload set allows to write basic data to the MessagePack
+   */
+
+  /*! Write null */
+  void write();
+
+  /*! Write a bool */
+  void write(bool b);
+
+  /*! Write an int8_t */
+  void write(int8_t i);
+
+  /*! Write an int16_t */
+  void write(int16_t i);
+
+  /*! Write an int32_t */
+  void write(int32_t i);
+
+  /*! Write an int64_t */
+  void write(int64_t i);
+
+  /*! Write a uint8_t */
+  void write(uint8_t i);
+
+  /*! Write a uint16_t */
+  void write(uint16_t i);
+
+  /*! Write a uint32_t */
+  void write(uint32_t i);
+
+  /*! Write a uint64_t */
+  void write(uint64_t i);
+
+  /*! Write a float */
+  void write(float f);
+
+  /*! Write a double */
+  void write(double d);
+
+  /*! Write an std::string */
+  void write(const std::string &s);
+
+  /*! Write a C-style string */
+  void write(const char *s);
+
+  /*! Write a C-style string with available size */
+  void write(const char *s, size_t len);
+
+  /*! Write an Eigen::Vector2d
+   *
+   * Serializes as an array of size 2
+   */
+  void write(const Eigen::Vector2d &v);
+
+  /*! Write an Eigen::Vector3d
+   *
+   * Serializes as an array of size 3
+   */
+  void write(const Eigen::Vector3d &v);
+
+  /*! Write an Eigen::VectorXd
+   *
+   * Serializes as an array of size X
+   */
+  void write(const Eigen::VectorXd &v);
+
+  /*! Write an Eigen::Quaterniond
+   *
+   * Serializes as an array of size X
+   */
+  void write(const Eigen::Quaterniond &q);
+
+  /*! Write an Eigen::Matrix3d
+   *
+   * Serializes as an array of size 9
+   */
+  void write(const Eigen::Matrix3d &m);
+
+  /*! Add data to the MessagePack (containers)
+   *
+   * These functions support the serialization of standard containers of
+   * serializable objects.
+   */
+
+  /*! Write an std::vector<T, A> */
+  template <typename T, typename A>
+  void write(const std::vector<T, A> &v) {
+    start_array(v.size());
+    for (const auto &value : v) {
+      write(value);
+    }
+    finish_array();
+  }
+
+  /*! Write an std::array<T, N> */
+  template <typename T, std::size_t N>
+  void write(const std::array<T, N> &a) {
+    start_array(N);
+    for (const auto &value : a) {
+      write(value);
+    }
+    finish_array();
+  }
+
+  /*! Write an std::pair<T1, T2> */
+  template <typename T1, typename T2>
+  void write(const std::pair<T1, T2> &p) {
+    start_array(2);
+    write(p.first);
+    write(p.second);
+    finish_array();
+  }
+
+  /*! Write an std::map<KeyT, T, C, A> */
+  template <typename KeyT, typename T, typename C, typename A>
+  void write(const std::map<KeyT, T, C, A> &m) {
+    start_map(m.count());
+    for (const auto &p : m) {
+      write(p.first);
+      write(p.second);
+    }
+    finish_map();
+  }
+
+  /*! Write an std::set<T, C, A> */
+  template <typename T, typename C, typename A>
+  void write(const std::set<T, C, A> &s) {
+    start_array(s.size());
+    for (const auto &value : s) {
+      write(value);
+    }
+    finish_array();
+  }
+
+  /*! Write an std::tuple<Args...> */
+  template <typename... Args>
+  void write(const std::tuple<Args...> &t) {
+    start_array(sizeof...(Args));
+    write_impl<0>(t);
+    finish_array();
+  }
+
+  /*! Start serializing an array
+   *
+   * \param size Size of the array
+   */
+  void start_array(size_t size);
+
+  /*! Finished serializing an array */
+  void finish_array();
+
+  /*! Start serializing a map
+   *
+   * \param size Size of the map, the map must then contains 2*size elements
+   */
+  void start_map(size_t size);
+
+  //! Finished serializing a map.
+  void finish_map();
+
+  /*! Write an existing object into the object being constructed.
+   *
+   * \param data Data written into the object
+   * \param size Size of the data
+   */
+  void write_object(const char *data, size_t s);
+
+  /*! Finish building the message. Data cannot be appended afterwards.
+   *
+   * \return Effective size of MessagePack data. Note that buffer.size() is
+   * likely different.
+   */
+  size_t finish();
+
+  //! Get pointer to the MPack writer for use with the C API
+  mpack_writer_t *mpack_writer() { return &writer_; }
+
+ private:
+  //! Internal MPack writer
+  mpack_writer_t writer_;
+
+  //! Internal write function for tuples
+  template <size_t i, typename... Args,
+            typename std::enable_if<i<sizeof...(Args), int>::type = 0> void
+                write_impl(const std::tuple<Args...> &t) {
+    write(std::get<i>(t));
+    write_impl<i + 1>(t);
+  }
+
+  //! Internal write function for tuples
+  template <size_t i, typename... Args,
+            typename std::enable_if<i >= sizeof...(Args), int>::type = 0>
+  void write_impl(const std::tuple<Args...> &) {}
+};
+
+}  // namespace palimpsest
---
+++ src/MessagePackWriter.cpp
@@ -0,0 +1,239 @@
+/*
+ * Copyright 2022 Stéphane Caron
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * This file incorporates work covered by the following copyright and
+ * permission notice:
+ *
+ *     MessagePackBuilder from https://github.com/jrl-umi3218/mc_rtc/
+ *     Copyright 2015-2019 CNRS-UM LIRMM, CNRS-AIST JRL
+ *     License: BSD-2-Clause
+ */
+
+#include "palimpsest/MessagePackWriter.h"
+
+#include <mpack.h>
+
+#include <string>
+#include <vector>
+
+#if not EIGEN_VERSION_AT_LEAST(3, 2, 90)
+namespace Eigen {
+using Index = Eigen::DenseIndex;
+}
+#endif
+
+/*! Intrusive flush function for std::vector.
+ *
+ * This is an intrusive flush function which modifies the writer's buffer in
+ * response to a flush instead of emptying it in order to add more capacity for
+ * data. This removes the need to copy data from a fixed buffer into a growable
+ * one, improving performance.
+ *
+ * There are three ways flush can be called:
+ * - flushing the buffer during writing (used is zero, count is all data, data
+ *   is buffer)
+ * - flushing extra data during writing (used is all flushed data, count is
+ *   extra data, data is not buffer)
+ * - flushing during teardown (used and count are both all flushed data, data
+ *   is buffer)
+ *
+ * In the first two cases, we grow the buffer by at least double, enough
+ * to ensure that new data will fit. We ignore the teardown flush.
+ *
+ * \note Inspired by mpack.c @ version 1.0
+ */
+static void mpack_std_vector_writer_flush(mpack_writer_t *writer,
+                                          const char *data, size_t count) {
+  auto &buffer = *static_cast<std::vector<char> *>(writer->context);
+
+  if (data == writer->buffer) {
+    if (mpack_writer_buffer_used(writer) == count) {
+      // teardown, do nothing
+      return;
+    }
+    // otherwise leave the data in the buffer and just grow
+    writer->current = writer->buffer + count;
+    count = 0;
+  }
+
+  size_t used = mpack_writer_buffer_used(writer);
+  size_t size = mpack_writer_buffer_size(writer);
+
+  mpack_log("flush size %i used %i data %p buffer %p\n",
+            static_cast<int>(count), static_cast<int>(used), data,
+            writer->buffer);
+
+  mpack_assert(
+      data == writer->buffer || used + count > size,
+      "extra flush for %i but there is %i space left in the buffer! (%i/%i)",
+      static_cast<int>(count),
+      static_cast<int>(mpack_writer_buffer_left(writer)),
+      static_cast<int>(used), static_cast<int>(size));
+
+  // grow to fit the data
+  // TODO(gergondet): this really needs to correctly test for overflow
+  size_t new_size = size * 2;
+  while (new_size < used + count) {
+    new_size *= 2;
+  }
+
+  mpack_log("flush growing buffer size from %i to %i\n", static_cast<int>(size),
+            static_cast<int>(new_size));
+
+  // grow the buffer
+  buffer.resize(new_size);
+  char *new_buffer = buffer.data();
+  if (new_buffer == NULL) {
+    mpack_writer_flag_error(writer, mpack_error_memory);
+    return;
+  }
+  writer->current = new_buffer + used;
+  writer->buffer = new_buffer;
+  writer->end = writer->buffer + new_size;
+
+  // append the extra data
+  if (count > 0) {
+    mpack_memcpy(writer->current, data, count);
+    writer->current += count;
+  }
+
+  mpack_log("new buffer %p, used %i\n", new_buffer,
+            static_cast<int>(mpack_writer_buffer_used(writer)));
+}
+
+namespace palimpsest {
+
+MessagePackWriter::MessagePackWriter(std::vector<char> &buffer) {
+  if (buffer.size() == 0) {
+    buffer.resize(MPACK_BUFFER_SIZE);
+  }
+  mpack_writer_init(&writer_, buffer.data(), buffer.size());
+  mpack_writer_set_context(&writer_, &buffer);  // used in flush function
+  mpack_writer_set_flush(&writer_, mpack_std_vector_writer_flush);
+}
+
+MessagePackWriter::~MessagePackWriter() {}
+
+void MessagePackWriter::write() { mpack_write_nil(&writer_); }
+
+void MessagePackWriter::write(bool b) { mpack_write_bool(&writer_, b); }
+
+void MessagePackWriter::write(int8_t i) { mpack_write_i8(&writer_, i); }
+
+void MessagePackWriter::write(int16_t i) { mpack_write_i16(&writer_, i); }
+
+void MessagePackWriter::write(int32_t i) { mpack_write_i32(&writer_, i); }
+
+void MessagePackWriter::write(int64_t i) { mpack_write_i64(&writer_, i); }
+
+void MessagePackWriter::write(uint8_t i) { mpack_write_u8(&writer_, i); }
+
+void MessagePackWriter::write(uint16_t i) { mpack_write_u16(&writer_, i); }
+
+void MessagePackWriter::write(uint32_t i) { mpack_write_u32(&writer_, i); }
+
+void MessagePackWriter::write(uint64_t i) { mpack_write_u64(&writer_, i); }
+
+void MessagePackWriter::write(float f) { mpack_write_float(&writer_, f); }
+
+void MessagePackWriter::write(double d) { mpack_write_double(&writer_, d); }
+
+void MessagePackWriter::write(const std::string &s) {
+  mpack_write_str(&writer_, s.c_str(), static_cast<uint32_t>(s.size()));
+}
+void MessagePackWriter::write(const char *s) { mpack_write_cstr(&writer_, s); }
+
+void MessagePackWriter::write(const char *s, size_t len) {
+  mpack_write_str(&writer_, s, static_cast<uint32_t>(len));
+}
+
+namespace {
+
+template <typename T>
+inline void write_vector(mpack_writer_t *writer, const T &v) {
+  for (Eigen::Index i = 0; i < v.size(); ++i) {
+    mpack_write_double(writer, v(i));
+  }
+}
+
+template <typename T>
+inline void write_matrix(mpack_writer_t *writer, const T &m) {
+  for (Eigen::Index i = 0; i < m.rows(); ++i) {
+    for (Eigen::Index j = 0; j < m.cols(); ++j) {
+      mpack_write_double(writer, m(i, j));
+    }
+  }
+}
+
+}  // namespace
+
+void MessagePackWriter::write(const Eigen::Vector2d &v) {
+  start_array(2);
+  write_vector(&writer_, v);
+  finish_array();
+}
+
+void MessagePackWriter::write(const Eigen::Vector3d &v) {
+  start_array(3);
+  write_vector(&writer_, v);
+  finish_array();
+}
+
+void MessagePackWriter::write(const Eigen::VectorXd &v) {
+  start_array(static_cast<size_t>(v.size()));
+  write_vector(&writer_, v);
+  finish_array();
+}
+
+void MessagePackWriter::write(const Eigen::Quaterniond &q) {
+  start_array(4);
+  write(q.w());
+  write(q.x());
+  write(q.y());
+  write(q.z());
+  finish_array();
+}
+
+void MessagePackWriter::write(const Eigen::Matrix3d &m) {
+  start_array(9);
+  write_matrix(&writer_, m);
+  finish_array();
+}
+
+void MessagePackWriter::start_array(size_t s) {
+  mpack_start_array(&writer_, static_cast<uint32_t>(s));
+}
+
+void MessagePackWriter::finish_array() { mpack_finish_array(&writer_); }
+
+void MessagePackWriter::start_map(size_t s) {
+  mpack_start_map(&writer_, static_cast<uint32_t>(s));
+}
+
+void MessagePackWriter::finish_map() { mpack_finish_map(&writer_); }
+
+void MessagePackWriter::write_object(const char *data, size_t s) {
+  mpack_write_object_bytes(&writer_, data, s);
+}
+
+size_t MessagePackWriter::finish() {
+  if (mpack_writer_destroy(&writer_) != mpack_ok) {
+    mpack_log("Failed to write to MessagePack");
+    return 0;
+  }
+  return mpack_writer_buffer_used(&writer_);
+}
+
+}  // namespace palimpsest
