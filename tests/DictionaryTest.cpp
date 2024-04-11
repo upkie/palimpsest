4a17f31b45e02e18216b873991e383b1bff39c07
Stéphane Caron <stephane.caron@inria.fr>
2024-04-11 10:28:08 +0200

    [minor] Compress source headers with SPDX license identifiers

--- tests/DictionaryTest.cpp
+++ tests/DictionaryTest.cpp
@@ -1,24 +1,12 @@
+// Copyright 2022 Stéphane Caron
+// SPDX-License-Identifier: Apache-2.0
 /*
- * Copyright 2022 Stéphane Caron
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy
- * of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- *
  * This file incorporates work covered by the following copyright and
  * permission notice:
  *
  *     Configuration and DataStore classes of mc_rtc
  *     Copyright 2015-2020 CNRS-UM LIRMM, CNRS-AIST JRL
- *     License: BSD-2-Clause
+ *     SPDX-License-Identifier: BSD-2-Clause
  */
 
 #include <gtest/gtest.h>
